e036db6d851015d565b0523da2bea936932ac774
afrizalr22 <afrizalr933@gmail.com>
2025-09-23 16:33:27 +0700

    Update Tailwind ke versi 4.1.13 serta configurasinya

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
-import './bootstrap';
-
-import Alpine from 'alpinejs';
+import "../css/app.css";
+import "./bootstrap";
+import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
 
--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -1,17 +1,21 @@
 <!doctype html>
 <html>
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="{{ asset('CSS/output.css') }}" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-  <!-- CSS for carousel/flickity-->
-  <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
-  <link rel="stylesheet" href="https://unpkg.com/flickity-fade@2/flickity-fade.css">
-  
-  <!-- CSS for modal/flowbite -->
-  <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css"  rel="stylesheet" /> -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('CSS/output.css') }}" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
+        rel="stylesheet" />
+    <!-- CSS for carousel/flickity-->
+    <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/flickity-fade@2/flickity-fade.css">
+
+    <!-- CSS for modal/flowbite -->
+    <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css"  rel="stylesheet" /> -->
 </head>
+
 <body class="font-poppins text-cp-black">
 
     @yield('content')
@@ -22,4 +26,5 @@
 
 
 </body>
+
 </html>
